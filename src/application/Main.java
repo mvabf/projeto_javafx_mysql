96f7661cfe5e9543e0c4ecd6a95fad0b4d296f3a
Marcus Venicius <mvabf_dev@outlook.com>
2020-02-10 17:14:26 -0300

    about view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,16 +9,18 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene mainScene = new Scene(scrollPane);
 			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
+			 mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -26,7 +28,11 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
